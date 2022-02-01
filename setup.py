f411b1ee2571b4548eebf553b0a98f42b33f4fac
arun-strollby <89654966+arun-strollby@users.noreply.github.com>
2022-02-01 15:28:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ else:
     define_macros=[
             ("WITH_GREENLET",None),
             ("HTTP_PARSER_DEBUG", "0") ]
-    install_requires=['greenlet>=0.4.5,<0.5']
+    install_requires=['greenlet>=1.0.2']
 
 if develop:
     define_macros.append(("DEVELOP",None))
